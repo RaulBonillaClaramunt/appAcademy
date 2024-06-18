ad9823c719d11eb79663aeb9b7e1e62c0e0e54f8
Raul <raulbonillaclaramunt@email.com>
2024-06-19 08:32:10 +0900

    kickstart exercise 12

--- practice-for-week-02-scope-problems-without-context-main/problems/12-volume.js
+++ practice-for-week-02-scope-problems-without-context-main/problems/12-volume.js
@@ -4,10 +4,10 @@ for a rectangle (height * width * length). In order to enter the
 required measurements we'll need to measure them one at a time.
 
 Write a function named `recVolume(height)`. The recVolume function will be passed
-a height int and will return a function. The function returned by recVolume can 
+a height int and will return a function. The function returned by recVolume can
 then be invoked two more times with a single argument number each time (one for
 length and one for width). Once all three numbers (height, width, length) have
-been collected return the volume of the rectangle. Any subsequent calls to the 
+been collected return the volume of the rectangle. Any subsequent calls to the
 function returned by recVolume should continue to return the original volume.
 
 Example 1:
@@ -24,8 +24,16 @@ console.log(table2(75)); // STILL prints 6
 
 ***********************************************************************/
 function recVolume(height) {
-  // Your code here
+  return returnFunc1 = (width) => {
+    return returnFunc2 = (lenght) => {
+      return height * width * length;
+    }
+  }
 }
+
+let table1 = recVolume(5); // returns a function
+table1(4); // returns a function
+console.log(table1(3)); // prints 60
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
