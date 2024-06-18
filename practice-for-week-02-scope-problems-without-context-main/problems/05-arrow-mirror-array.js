ea4330bca167d36971af42443d1c7c1ece92ffd2
Raul <raulbonillaclaramunt@email.com>
2024-06-18 11:18:13 +0900

    testing github

---
+++ practice-for-week-02-scope-problems-without-context-main/problems/05-arrow-mirror-array.js
@@ -0,0 +1,27 @@
+/***********************************************************************
+Write a function `mirrorArray(array)` that takes in an array as an
+argument and returns a new array "mirrored" as shown in the examples:
+
+Examples:
+
+mirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
+mirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+***********************************************************************/
+
+const mirrorArray = (array) => {
+  let mirrow = array;
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    mirrow.push(array[i]);
+  }
+
+  return mirrow;
+};
+
+//console.log(mirrorArray([1,2,3])); // => [ 1, 2, 3, 3, 2, 1 ]
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+  module.exports = mirrorArray;
+} catch (e) {
+  return null;
+}
