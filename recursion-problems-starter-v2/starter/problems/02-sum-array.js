b2847f026f30aa1c4c403e9334ce82e433d7133c
Raul <raulbonillaclaramunt@email.com>
2024-06-26 08:24:15 +0900

    exercise2 solved

--- recursion-problems-starter-v2/starter/problems/02-sum-array.js
+++ recursion-problems-starter-v2/starter/problems/02-sum-array.js
@@ -14,14 +14,14 @@ sumArray([1, 2, 3, 4, 5]); // 15
 - recursive step
 */
 function sumArray(array) {
-  if (array.length < 0) return 0;
+  if (array.length == 0) return 0;
 
-  return array[array.length -1] + sumArray(array.pop);
+  return array[0] + sumArray(array.slice(1));
 
 }
 
-sumArray([1, 2, 3]); //  6
-sumArray([0, 1, -3]); //  -2
+console.log(sumArray([1, 2, 3])); //  6
+console.log(sumArray([0, 1, -3])); //  -2
 sumArray([1, 2, 3, 4, 5]); // 15
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
