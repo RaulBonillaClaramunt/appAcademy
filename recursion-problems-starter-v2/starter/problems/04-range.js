2ef12283d09ee246e4b2aff24427a7b9d14b7829
Raul <raulbonillaclaramunt@email.com>
2024-06-26 09:13:12 +0900

    exercise 4 solved

--- recursion-problems-starter-v2/starter/problems/04-range.js
+++ recursion-problems-starter-v2/starter/problems/04-range.js
@@ -11,22 +11,19 @@ range(7, 6); // []
 ***********************************************************************/
 
 function range(start, end) {
-  if (start < end) return [];
 
-  let array = [];
-
-  array.push(start);
-  console.log(array);
-  if (start === end - 1) return [];
-
-  if (start > end) return array;
-
-  return range(start + 1, end);
+  if (start >= end) {
+    return [];
+  } else {
+    return [start].concat(range(start + 1, end));
+  }
 }
+
+
 
 console.log(range(1, 5)); // [1, 2, 3, 4]
 console.log('-------');
-range(3, 4); // [3]
+console.log(range(3, 4)); // [3]
 console.log('-------');
 console.log(range(7, 6)); // []
 
