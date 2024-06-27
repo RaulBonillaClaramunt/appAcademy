33b218fba71a862631a18ee89a08fea5280eed25
Raul <raulbonillaclaramunt@email.com>
2024-06-27 23:33:13 +0900

    exercise 06-add-to-twelve solved recursively

--- recursion-problems-starter-v2/starter/problems/06-add-to-twelve.js
+++ recursion-problems-starter-v2/starter/problems/06-add-to-twelve.js
@@ -11,9 +11,45 @@ addToTwelve([1, 11, 4, 7, 6]); // true
 addToTwelve([1, 12, 4, 7, 6]); // false
 addToTwelve([1]); // false
 ***********************************************************************/
+//for (let i = 0; i < arr.length; i++) => if (i + (i + 1) = 12) => return true
+/*
+let addToTwelve = (arr, i = 0) => {
 
-// your code here
+  if (arr[i] + arr[i + 1] === 12) {
+    //console.log(true);
+    return true;
+  }
 
+  if (arr.length < 2) {
+    //console.log(false);
+    return false;
+  } else {
+    arr.shift();
+    return addToTwelve(arr, i++) ;
+  }
+}
+*/
+let addToTwelve = (arr) => {
+
+  if (arr[0] + arr[1] === 12) {
+    console.log(true);
+    return true;
+  }
+
+  if (arr.length < 2) {
+    console.log(false);
+    return false;
+  } else {
+    arr.shift();
+    return addToTwelve(arr) ;
+  }
+}
+
+addToTwelve([1, 3, 4, 7, 5]); // true
+addToTwelve([1, 3, 4, 7, 6]); // false
+addToTwelve([1, 11, 4, 7, 6]); // true
+addToTwelve([1, 12, 4, 7, 6]); // false
+addToTwelve([1]); // false
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = addToTwelve;
