976547f65863a02e0448adaf3b9de7d543436098
Raul <raulbonillaclaramunt@email.com>
2024-06-28 08:38:35 +0900

    09-fibonacci solved with recursion

--- recursion-problems-starter-v2/starter/problems/07-exponent.js
+++ recursion-problems-starter-v2/starter/problems/07-exponent.js
@@ -25,11 +25,11 @@ function exponent(b, n) {
     return (1 / b) * exponent(b, n + 1);
   }
 }
-
+/*
 console.log(exponent(3, 2)); // 9
 console.log(exponent(2, 3)); // 8
 console.log(exponent(2, -2)); // 1/4 (or 0.25)
-console.log(exponent(2, -3)); // 1/4 (or 0.25)
+console.log(exponent(2, -3)); // 1/4 (or 0.25)*/
 console.log(exponent(5, 5)); // 3125
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
