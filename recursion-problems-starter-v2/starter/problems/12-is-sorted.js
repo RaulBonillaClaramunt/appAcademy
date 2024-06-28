b9f4762730258089df761e76b21cc66b3b132423
Raul <raulbonillaclaramunt@email.com>
2024-06-28 23:59:22 +0900

    coding 12-is-sorted solution

--- recursion-problems-starter-v2/starter/problems/12-is-sorted.js
+++ recursion-problems-starter-v2/starter/problems/12-is-sorted.js
@@ -9,8 +9,27 @@ isSorted([1, 2, 4, 3, 5]); // false
 isSorted([2, 4, 6, 7, 8]); // true
 isSorted([5, 4, 3, 2, 1]); // false
 ***********************************************************************/
+//if i < (i + 1 ) recursive true
+//if arr[array.length - 1] < arr[array.length] return
+function isSorted(array, i = 0) {
+    if (i === (array.length - 1)) return true;
 
-// your code here
+    if (array[i] > array[i + 1]) return false;
+
+    else {
+      (array[i] < array[i + 1])
+      return isSorted(array, i++);
+    }
+
+    //return true;
+  //}
+
+}
+
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+isSorted([1, 2, 4, 3, 5]); // false
+isSorted([2, 4, 6, 7, 8]); // true
+isSorted([5, 4, 3, 2, 1]); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
