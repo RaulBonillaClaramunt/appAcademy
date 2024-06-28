2b164c8c49277bae691eaad93b85fb80f02674d7
Raul <raulbonillaclaramunt@email.com>
2024-06-29 08:28:27 +0900

    first attempt on debugging recursive-debug

--- skeleton/recursive-debug.js
+++ skeleton/recursive-debug.js
@@ -14,8 +14,13 @@ doForAll(["a", "b", "c"]), (x) => x.toUpperCase()); // => ["A", "B", "C"]
 ***********************************************************************/
 
 function doForAll(arr, action) {
+  if (arr.length === 0) return [];
   return [action(arr[0]), ...doForAll(arr.slice(1), action)];
 }
+
+console.log(doForAll([], (x) => x * 2)); // => []
+doForAll([1, 2, 3], (x) => x + 1); // => [2, 3, 4]
+doForAll(["a", "b", "c"]), (x) => x.toUpperCase(); // => ["A", "B", "C"]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = doForAll;
