5fde8f7b7194a13cdd2fb9c093c8cec53ee6bd30
Raul <raulbonillaclaramunt@email.com>
2024-07-20 12:09:38 +0900

    timeout exercise 3 solved

--- practice-for-week-03-timeout-pt1-practice/problems/03-batch-timeouts.js
+++ practice-for-week-03-timeout-pt1-practice/problems/03-batch-timeouts.js
@@ -33,8 +33,35 @@ console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 ***********************************************************************/
 
 function batchTimeouts(callbacks, delays) {
-    // Your code here
+    let result = [];
+
+    for (let i = 0; i < callbacks.length; i++) {
+      setTimeout(() => {
+        callbacks[i]();
+      }, delays[i]);
+      const resultObject = {};
+      const key = callbacks[i]();
+      const value = delays[i];
+      resultObject.key = value;
+      result.push(resultObject);
+    }
+
+    return result;
   }
+
+  const sayHello = () => console.log('hi');
+  const sayGoodbye = () => console.log('bye');
+  const shout = () => console.log('WHAT?');
+  const tasks = [sayHello, sayGoodbye, shout];
+  const delays = [500, 200, 900];
+
+  const timeoutObjs = batchTimeouts(tasks, delays);
+  // should print:
+  //  'bye' after 200 ms
+  //  'hi' after 500 ms
+  //  'WHAT?' after 900 ms
+
+  console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 
   /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
   try {
