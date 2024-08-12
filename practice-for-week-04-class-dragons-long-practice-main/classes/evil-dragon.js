6f44d3554ca146845d91be02e1ed71a465bd2574
Raul <raulbonillaclaramunt@email.com>
2024-08-12 08:42:24 +0800

    third phase of long classes practice solved

--- practice-for-week-04-class-dragons-long-practice-main/classes/evil-dragon.js
+++ practice-for-week-04-class-dragons-long-practice-main/classes/evil-dragon.js
@@ -1,1 +1,35 @@
-// Your code here
+const Dragon = require('./dragon');
+
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    };
+
+    dontInviteThemOverForDinner() {
+        for (const evilDoing of this.evilDoings) {
+            console.log(`${this.name} will ${evilDoing}`);
+        };
+    };
+
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+    };
+};
+
+module.exports = EvilDragon;
+
+const enderDragon = new EvilDragon(
+    "Ender Dragon",
+    "black", [
+      "spit a fireball at Steve",
+      "rule over all the Endermen",
+      "destroy all blocks in its way"
+    ],
+    "Steve"
+  );
+  console.log(enderDragon);
+  enderDragon.dontInviteThemOverForDinner();
+  console.log(enderDragon.breathesFire());
+  console.log(enderDragon.burnsNemesis());
